523e5032447476386a4785eca73ca2c7a77163b9
zhiziwj <zhizi6@tutamail.com>
2025-06-07 21:10:03 +0800

    进行第一版上传

---
+++ src.h
@@ -0,0 +1,62 @@
+#ifndef math_pro
+#define math_pro
+const double PI=3.1415926;
+const double eps=0.0001;
+long long int pow(long long int base,long long int power){
+	if(power==1){
+		return base;
+	}
+	long long int ans=1;
+	while(power>0){
+		if(power&1){
+			ans*=base;
+		}
+		base*=base;
+		power>>=1;
+	}
+	return ans;
+}
+long long int fac(long long int base){
+	long long int ans=1;
+	for(int i=base;i>=2;i--){
+		ans*=i;
+	}
+	return ans;
+}
+long long int log2(long long int n){
+	long long int ans=0;
+	while(n>1){
+		n/=2;
+		ans++;
+	}
+	return ans;
+}
+long long int log10(long long int n){
+	long long int ans=0;
+	while(n>1){
+		n/=10;
+		ans++;
+	}
+	return ans;
+}
+long long int abs_(long long int x){
+	if(x>=0){
+		return x;
+	}
+	return -x;
+}
+double abs_double(double x){
+	if(x>=0){
+		return x;
+	}
+	return -x;
+}
+double sqrt(double x){
+	double ans=x,last=-1.00;
+	while(abs_double(ans-last)>eps){
+		last=ans;
+		ans=(ans+x/ans)/2;
+	}
+	return ans;
+}
+#endif
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include "src.h"
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<PI<<" "<<eps<<endl;
+	cout<<pow(2,10)<<" "<<fac(3)<<endl;
+	cout<<log2(1024)<<" "<<log10(100)<<endl;
+	cout<<abs_(-1024)<<" "<<sqrt(4)<<endl;
+	return 0;
+}
