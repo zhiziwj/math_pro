44915417b11f841e8c9421abb32e8637d00777bd
zhiziwj <zhizi6@tutamail.com>
2025-06-08 15:45:36 +0800

    添加对复数运算的测试

--- test.cpp
+++ test.cpp
@@ -6,5 +6,9 @@ int main(){
 	cout<<pow(2,10)<<" "<<fac(3)<<endl;
 	cout<<log2(1024)<<" "<<log10(100)<<endl;
 	cout<<abs_(-1024)<<" "<<sqrt(4)<<endl;
+	complex a,b;
+	a={1,1};
+	b={3,3};
+	cout<<add(a,b).real<<" "<<add(a,b).imag<<endl;
 	return 0;
 }
